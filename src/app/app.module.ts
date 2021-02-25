2d35c00068db884ea6f4a04bdb412faddbb5fceb
YangDreamy <yangmengwei1997@126.com>
2021-02-25 14:38:32 +0100

    route change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,14 @@ import { HomeComponent } from './components/home/home.component';
 import { RecipeComponent } from './components/recipe/recipe.component';
 import { CommonModule }     from '@angular/common';
 import { FileUploadModule } from 'ng2-file-upload';
-<<<<<<< HEAD
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule} from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import {RouterModule} from '@angular/router';
-
-=======
+import { appRoutes } from './app.routes';
 import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
-import { RouterModule } from '@angular/router';
->>>>>>> 617611fab1c29dd3e0550600b7c0d9b1fb4da95c
 @NgModule({
   declarations: [
     AppComponent,
@@ -32,19 +28,12 @@ import { RouterModule } from '@angular/router';
     AppRoutingModule,
     CommonModule,
     FileUploadModule,
-<<<<<<< HEAD
     MatButtonModule,
     MatMenuModule,
     MatToolbarModule,
     MatIconModule,
     MatCardModule,
-    // RouterModule.forRoot(AppRoutes) 
-=======
-    RouterModule.forRoot([
-      {path: '', component: HomeComponent},
-      {path: 'components/shopping-list', component: ShoppingListComponent}
-    ])
->>>>>>> 617611fab1c29dd3e0550600b7c0d9b1fb4da95c
+    RouterModule.forRoot(appRoutes) 
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/recipe/recipe.component.ts
+++ src/app/components/recipe/recipe.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-recipe',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _activitedRoute : ActivatedRoute) { }
 
   ngOnInit(): void {
   }
